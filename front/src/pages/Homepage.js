f5a12128752307f14cc03c803be87d6202b1ee00
Aleksandr130402 <62347833+Aleksandr130402@users.noreply.github.com>
2024-02-01 17:21:50 +0100

    added gql query to ReviewDetails.js

--- front/src/pages/Homepage.js
+++ front/src/pages/Homepage.js
@@ -3,6 +3,20 @@ import { Link } from "react-router-dom";
 import { useQuery, gql } from "@apollo/client";
 
 export default function Homepage() {
+	const REVIEWS = gql`
+		query GetReviews {
+			reviews {
+				data {
+					id
+					attributes {
+						title
+						rating
+						body
+					}
+				}
+			}
+		}
+	`;
 	const { data, loading, error } = useQuery(REVIEWS);
 	if (loading) return <p>"Loading..."</p>;
 	if (error) return <p>Error happened:(</p>;
--- front/src/pages/ReviewDetails.js
+++ front/src/pages/ReviewDetails.js
@@ -1,20 +1,48 @@
+import { useQuery, gql } from "@apollo/client";
 import React from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export default function ReviewDetails() {
-	// prettier-ignore
-	const REVIEW = gql`
-		query GetReviews {
-			reviews {
-				data {
-					id,
-					attributes {
-						title,
-						rating,
-						body
-					}
+const REVIEW = gql`
+	query GetReview($id: ID!) {
+		review(id: $id) {
+			data {
+				id
+				attributes {
+					title
+					rating
+					body
 				}
 			}
 		}
-	`;
-	return <div>Details</div>;
+	}
+`;
+
+export default function ReviewDetails() {
+	const { id } = useParams();
+	const { data, loading, error } = useQuery(REVIEW, {
+		variables: { id: id },
+	});
+
+	if (loading) return <p>"Loading..."</p>;
+	if (error) return <p>Error happened:(</p>;
+
+	console.log(data);
+	const item = data.review.data;
+	return (
+		<div>
+			<div key={item.id} className="review-card">
+				<div className="rating"></div>
+				<h2>{item.attributes.title}</h2>
+				<small>console list</small>
+				<div>
+					{item.attributes.body.map((paragraph) => (
+						<p>{paragraph.children[0].text}</p>
+					))}
+				</div>
+
+				<Link to="/">Go back</Link>
+			</div>
+		</div>
+	);
 }
